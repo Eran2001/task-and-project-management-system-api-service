9bb193ec848c801bcafde1e5616577ac3e4804f5
M-E-Hasareli <eranhasareli@moneta.lk>
2025-11-03 10:20:21 +0530

    add post request

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+app.post("/users", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please enter your username and password" });
+  }
+
+  console.log(`Welcome onboard ${username}`);
+  return res.status(200).json({ message: `Welcome onboard ${username}` });
 });
 
 app.listen(PORT, () => {
