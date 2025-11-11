a8ac0f8d142f4bea0182e534914cdbe72a956bc5
M Eran Hasareli <eran.hasarali.2001@gmail.com>
2025-11-11 21:20:26 +0530

    config some endpoints

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import connectDB from "./configs/db.js";
 
 import userRegisterRoutes from "./routes/auth/register/userRegisterRoutes.js";
@@ -11,9 +13,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/auth", userRegisterRoutes);
+app.use("/api/v1", userRegisterRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
